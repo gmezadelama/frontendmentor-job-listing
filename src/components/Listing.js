05cea56cac01df176ed623958f07f00fad1ed28c
Gustavo MDL <Admin@Administrators-MacBook-Pro.local>
2020-04-06 14:18:34 -0400

    Setting a default value of all the job ads only for the preview view on frontendmentor

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect, useContext } from 'react';
 import { DataContext } from '../context/DataContext';
 import { FilterContext } from '../context/FilterContext';
 import Job from './Job';
+// this is only for the preview view on frontend mentor, in a real app 
+// the default value should be an empty array
+import defaultData from '../api/data/data.json';
 
 function Listing(onAddFilter) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(defaultData);
   const getData = useContext(DataContext);
   const {
     filters
